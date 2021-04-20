425e70307ba94de985d002cfa8626ace82774082
alshum <alan.shum@appdynamics.com>
2021-04-19 20:35:28 -0700

    Fixed webworker.js so webworker will read js file

--- webworker.js
+++ webworker.js
@@ -1,5 +1,5 @@
-onmessage = function(event) {
+self.onmessage = function(event) {
 	var tmpValue = event.data;
 	var stackURLS = tmpValue.match(/([\\w+]+\\:\\/\\/)+([\\w\\d-]+\\.)*[\\w-]+[\\.\\:]\\w+([\\/\\?\\=\\&\\#.]?[\\w-]+)*\\/?/gm);
-	postMessage(stackURLS);
+	self.postMessage(stackURLS);
 }
